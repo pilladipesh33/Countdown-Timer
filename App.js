fcdf176f8c4ac743977ce6d194c045fc7ef56134
Dipesh <pilla.dipesh@quovantis.com>
2022-08-02 12:53:00 +0530

    countdown_animation_added

--- App.js
+++ App.js
@@ -27,18 +27,103 @@ const ITEM_SPACING = (width - ITEM_SIZE) / 2;
 
 const App = () => {
   const scrollX = React.useRef(new Animated.Value(0)).current;
+  const [duration, setDuration] = React.useState(timer[0]);
+  const inputRef = React.useRef();
+  const timerAnimation = React.useRef(new Animated.Value(height)).current;
+  const textInputAnimation = React.useRef(new Animated.Value(timer[0])).current;
+  const buttonAnimation = React.useRef(new Animated.Value(0)).current;
+
+  React.useEffect(() => {
+    const listener = textInputAnimation.addListener(({value}) => {
+      inputRef?.current?.setNativeProps({
+        text: Math.ceil(value).toString()
+      })
+    })
+
+    return () => {
+      textInputAnimation.removeListener(listener);
+      textInputAnimation.removeAllListeners();
+    }
+  })
+
+  const animation = React.useCallback(() => {
+    textInputAnimation.setValue(duration )
+    Animated.sequence([
+      Animated.timing(buttonAnimation, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true
+      }),
+      Animated.timing(timerAnimation, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true
+      }),
+      Animated.parallel([
+        Animated.timing(textInputAnimation, {
+          toValue: 0,
+          duration: duration * 1000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(timerAnimation, {
+          toValue: height,
+          duration: duration * 1000,
+          useNativeDriver: true,
+        })
+      ]),
+      Animated.delay(400)
+    ]).start(() => {
+      Vibration.cancel();
+      Vibration.vibrate();
+      textInputAnimation.setValue(duration);
+      Animated.timing(buttonAnimation, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true,
+      }).start()
+    })
+  }, [duration])
+
+  const opacity = buttonAnimation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [1, 0]
+  })
+
+  const translateY = buttonAnimation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 200]
+  })
+
+  const textOpacity = buttonAnimation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 1]
+  })
   return (
     <View style={styles.container}>
       <StatusBar hidden />
+      <Animated.View
+        style={[StyleSheet.absoluteFillObject, {
+          height,
+          width,
+          backgroundColor: colors.red,
+          transform: [{
+            translateY: timerAnimation
+          }]
+        }]}
+      />
       <Animated.View style={[
         StyleSheet.absoluteFillObject,
         {
           justifyContent: 'flex-end',
           alignItems: 'center',
           paddingBottom: 100,
+          opacity,
+          transform: [{
+            translateY,
+          }]
         }
       ]}>
-        <TouchableOpacity onPress={() => { }}>
+        <TouchableOpacity onPress={animation}>
           <View style={styles.roundButton} />
         </TouchableOpacity>
       </Animated.View>
@@ -49,6 +134,16 @@ const App = () => {
         right: 0,
         flex: 1,
       }}>
+        <Animated.View style={{
+          position: 'absolute',
+          width: ITEM_SIZE,
+          justifyContent: 'center',
+          alignSelf: 'center',
+          alignItems: 'center',
+          opacity: textOpacity
+        }}>
+          <TextInput ref={inputRef} style={styles.text} defaultValue={duration.toString()} />
+        </Animated.View>
         <Animated.FlatList
           data={timer}
           horizontal
@@ -57,16 +152,20 @@ const App = () => {
           contentContainerStyle={{
             paddingHorizontal: ITEM_SPACING
           }}
-          style={{flexGrow: 0}}
-          decelerationRate ='fast'
+          onMomentumScrollEnd={ev => {
+            const index = Math.round(ev.nativeEvent.contentOffset.x / ITEM_SIZE)
+            setDuration(timer[index]);
+          }}
+          style={{ flexGrow: 0, opacity}}
+          decelerationRate='fast'
           snapToInterval={ITEM_SIZE}
           onScroll={Animated.event(
-            [{nativeEvent: {contentOffset: {x: scrollX}}}], 
-            {useNativeDriver: true}
+            [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+            { useNativeDriver: true }
           )}
           keyExtractor={item => item.toString()}
           renderItem={({ item, index }) => {
-            const inputRange= [
+            const inputRange = [
               (index - 1) * ITEM_SIZE,
               index * ITEM_SIZE,
               (index + 1) * ITEM_SIZE,
@@ -74,12 +173,12 @@ const App = () => {
 
             const opacity = scrollX.interpolate({
               inputRange,
-              outputRange:  [.4, 1, .4]
+              outputRange: [.4, 1, .4]
             })
 
             const scale = scrollX.interpolate({
               inputRange,
-              outputRange:  [.7, 1, .7]
+              outputRange: [.7, 1, .7]
             })
             return (
               <View style={{ width: ITEM_SIZE, justifyContent: 'center', alignItems: 'center' }}>
